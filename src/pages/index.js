c9e70fc48d24bd79450d36728a98ab53dccc3769
gledsonssantos <gledsonssantos@gmail.com>
2019-12-25 13:31:55 -0300

    Textos e Imagens

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,13 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Olá masternodeiros!</h1>
     <p>Bem vindos, este será o site da ASABRANCA Serviços Digitais, iniciaremos nossas atividades em breve.</p>
-    <p>Estamos trabalando para que em Janeiro possamos aceitar a gestão de seus Maternodes MarteX.</p>
-    <p></p>
-    <p>Contatos via Telegram: @gledsonssantos.</p>
+    <p>Estamos trabalhando para que em Janeiro em 06/01/2020 possamos aceitar a gestão de seus Maternodes MarteX.</p>
+    <p>Contatos via Telegram: <a href="https://t.me/gledsonssantos">@gledsonssantos</a>.</p>
+    
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-#   <Link to="/page-2/">Go to page 2</Link>
+/**   <Link to="/page-2/">Go to page 2</Link> */
   </Layout>
 )
 
