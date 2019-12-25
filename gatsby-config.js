07189f5e730216c09bfc3acc750b79d56a549d9c
gledsonssantos <gledsonssantos@gmail.com>
2019-12-25 16:30:33 -0300

    Textos, Imagens e Links

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `ASABRANCA Serviços Digitais`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A sua Gestão Profissional de Masternodes.`,
+    author: `@gledsonssantos`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
