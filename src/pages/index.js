4cdd580541de4cf7bb6df24184ef7eb588177e73
gledsonssantos <gledsonssantos@gmail.com>
2019-12-25 12:58:52 -0300

    Textos

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,15 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Olá masternodeiros!</h1>
+    <p>Bem vindos, este será o site da ASABRANCA Serviços Digitais, iniciaremos nossas atividades em breve.</p>
+    <p>Estamos trabalando para que em Janeiro possamos aceitar a gestão de seus Maternodes MarteX.</p>
+    <p></p>
+    <p>Contatos via Telegram: @gledsonssantos.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+#   <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
