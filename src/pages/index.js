01f7150e25165db40cf13cf70e8821ae9792658d
gledsonssantos <gledsonssantos@gmail.com>
2019-12-25 14:52:34 -0300

    Textos, Imagens e Links

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,23 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-
+//
+    <script src="{% static 'js/vendors.min.js' %}"></script>
+    <script src="{% static 'js/jquery.time-to.min.js' %}"></script>
+    <script>
+        $(document).ready(function() {
+            $('#countdown').timeTo({
+                timeTo: new Date(new Date('Mon Jan 06 2020 09:00:00 GMT-0300 (Brasilia Standard Time)')),
+                displayDays: 2,
+                displayCaptions: true,
+                fontSize: 30,
+                captionSize: 14,
+                lang: 'pt'
+            });
+        });
+    </script>
+    </body>
+//
   </Layout>
 )
 
